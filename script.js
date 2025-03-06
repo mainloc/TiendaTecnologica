a18889bceb981f67e5636182662b392af789b1c1
mainloc <150938215+mainloc@users.noreply.github.com>
2025-03-06 18:02:39 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    M.AutoInit();
+
+    document.getElementById('form-inscripcion').addEventListener('submit', function (event) {
+        let nombre = document.getElementById('nombre').value.trim();
+        let email = document.getElementById('email').value.trim();
+        let check = document.getElementById('check').checked;
+
+        if (nombre === '' || email === '' || !check) {
+            event.preventDefault();
+            alert('Por favor, complete todos los campos y acepte la política de privacidad.');
+        }
+    });
+});
